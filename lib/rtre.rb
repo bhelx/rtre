187e98846a17417b905529b09a01e329091ddbc6
Benjamin Eckel <benjamin.eckel@gmail.com>
2012-01-10 18:56:19 -0600

    remove constants file, wanted to make smaller

--- lib/rtre.rb
+++ lib/rtre.rb
@@ -2,7 +2,6 @@ require 'ffi'
 
 require "rtre/version"
 require "rtre/base"
-require "rtre/constants"
 require "rtre/structs"
 require "rtre/functions"
 
--- lib/rtre/base.rb
+++ lib/rtre/base.rb
@@ -1,5 +1,27 @@
 module Rtre
   extend FFI::Library
   ffi_lib 'tre'
+
+  #https://github.com/GerHobbelt/libtre/blob/master/lib/tre.h#L109
+  # POSIX tre_regcomp() flags.
+  REG_EXTENDED = 1
+  REG_ICASE = (REG_EXTENDED << 1)
+  REG_NEWLINE = (REG_ICASE << 1)
+  REG_NOSUB = (REG_NEWLINE << 1)
+
+  # Extra tre_regcomp() flags.
+  REG_BASIC = 0
+  REG_LITERAL = (REG_NOSUB << 1)
+  REG_RIGHT_ASSOC = (REG_LITERAL << 1)
+  REG_UNGREEDY = (REG_RIGHT_ASSOC << 1)
+
+  # POSIX tre_regexec() flags.
+  REG_NOTBOL = 1
+  REG_NOTEOL = (REG_NOTBOL << 1)
+
+  # Extra tre_regexec() flags.
+  REG_APPROX_MATCHER = (REG_NOTEOL << 1)
+  REG_BACKTRACKING_MATCHER = (REG_APPROX_MATCHER << 1)
+
 end
 
